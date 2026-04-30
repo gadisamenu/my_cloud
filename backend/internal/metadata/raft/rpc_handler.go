45312808c08d20e1e92efb1e87c4ef78f22aee44
gadisamenu <amegadbay@gmail.com>
2026-04-30 07:59:33 +0300

    fix: index consistency after snapshot

--- backend/internal/metadata/raft/rpc_handler.go
+++ backend/internal/metadata/raft/rpc_handler.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -56,10 +57,8 @@ func (n * RaftNode) HandleAppendEntries(req AppendEntriesRequest) AppendEntriesR
 	}
 	// become follower
 	if req.Term > n.currentTerm {
-		n.currentTerm = req.Term
-		n.votedFor = nil
-		n.state = Follower
-		n.storage.SaveState(n.currentTerm, n.votedFor)
+		fmt.Printf("Stepping down latest term %d is found\n", req.Term )
+		n.stepDown(req.Term)
 		// n.persist()
 	}
 
@@ -67,49 +66,53 @@ func (n * RaftNode) HandleAppendEntries(req AppendEntriesRequest) AppendEntriesR
 
 	if req.PrevLogIndex < n.lastIncludedIndex {
 		// leader is too far behind
-		return AppendEntriesResponse{Success: false}
+		fmt.Println("Leader is too far behind")
+		return AppendEntriesResponse{ Success: false }
 	}
 
 	// check log consistency
-	if req.PrevLogIndex > len(n.log) {
-		return AppendEntriesResponse{ Term: n.currentTerm, Success:  false}
+	if req.PrevLogIndex > n.lastLogIndex(){
+		fmt.Println("Index didn't match")
+		return AppendEntriesResponse{ Term: n.currentTerm, Success:  false, ConflictIndex: n.lastLogIndex() + 1 }
 	}
 	
 	if req.PrevLogIndex > 0 && 
 		n.getLogTerm(req.PrevLogIndex) != req.PrevLogTerm {
-		 return AppendEntriesResponse{ Term: n.currentTerm, Success:  false}
+		fmt.Printf("Conflicting term prevLogTerm -> %d \n", req.PrevLogTerm)
+		return AppendEntriesResponse{ Term: n.currentTerm, Success:  false, ConflictIndex: req.PrevLogIndex }
 	}
 
 	// find first mismatch
-	i := 0
-	for ; i < len(req.Entries); i++ {
-		idx := req.PrevLogIndex + 1 + i
+	entryIdx := 0
+	for ; entryIdx < len(req.Entries); entryIdx++ {
+		index := req.PrevLogIndex + 1 + entryIdx
 
-		if idx > len(n.log) {
+		if index > n.lastLogIndex() {
 			break
 		}
 
-		if n.getLogTerm(idx) != req.Entries[i].Term {
+		if n.getLogTerm(index) != req.Entries[entryIdx].Term {
+			// truncate conflicting suffix
+			truncateIndex := req.PrevLogIndex - n.lastIncludedIndex + entryIdx
+			if truncateIndex < len(n.log) {
+				n.log = n.log[:truncateIndex]
+				n.storage.RewriteLog(n.log)
+			}
 			break
 		}
-	}
-
-	// truncate conflicting suffix
-	truncateIndex := req.PrevLogIndex + i
-	if truncateIndex < len(n.log) {
-		n.log = n.log[:truncateIndex]
-		n.storage.RewriteLog(n.log)
 	}
 
 	// append only NEW entries
-	newEntries := req.Entries[i:]
-	if len(newEntries) > 0 {
+	if entryIdx < len(req.Entries){
+		newEntries := req.Entries[entryIdx:]
+		
 		n.log = append(n.log, newEntries...)
 		n.storage.AppendLog(newEntries)
 	}
 
 	if req.LeaderCommit > n.commitIndex {
-		n.commitIndex = min(req.LeaderCommit, len(n.log))
+		fmt.Printf("Updating commitIndex, leader commit %d and current lastlogIndex %d \n", req.LeaderCommit, n.lastLogIndex() )
+		n.commitIndex = min(req.LeaderCommit, n.lastLogIndex())
 	}
 
 	return AppendEntriesResponse{ Term: n.currentTerm, Success: true } 
